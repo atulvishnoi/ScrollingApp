644c00840960d185f3d3197ce12af6a5b827c8a4
Atul Vishnoi <atul.vishnoi@nagarro.com>
2022-04-26 14:57:19 +0530

    Removed unused  return

--- ScrollingApp/ScrollingApp/Scenes/View/DetailViewController.swift
+++ ScrollingApp/ScrollingApp/Scenes/View/DetailViewController.swift
@@ -120,19 +120,19 @@ extension DetailViewController: UICollectionViewDelegateFlowLayout, UICollection
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.zero
+        UIEdgeInsets.zero
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 2.0
+        2.0
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
+        0.0
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: ScreenSize.width, height: sectionHeaderHeight)
+        CGSize(width: ScreenSize.width, height: sectionHeaderHeight)
     }
 }
 
